041dcc73cdaf1e3e009b2c211977e159f1045608
Joshua Banks <152246049+DansNewLegs@users.noreply.github.com>
2024-01-08 08:54:46 -0500

    Update runtipi-autoupdate.sh

--- runtipi-autoupdate.sh
+++ runtipi-autoupdate.sh
@@ -1,32 +1,46 @@
 #!/usr/bin/env bash
-
+ 
+# Function to compare semantic versions
+compare_major_version() {
+    local major_version1=$(echo $1 | cut -d. -f1)
+    local major_version2=$(echo $2 | cut -d. -f1)
+ 
+    if [[ "$major_version1" == "$major_version2" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+ 
 # Get Current Version of Runtipi
-file_path=/opt/runtipi # Change this to the path of your runtipi installation.
-current_version=$(cat $file_path/VERSION)
-current_version_number=$(echo $current_version | tr -cd '[:digit:]')
-
+runtipi_path=/path/to/runtipi # Change this to the path of your runtipi installation.
+current_version=$(cat $runtipi_path/VERSION)
+ 
 # Get the latest release information from GitHub API
 latest_release=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/runtipi/runtipi/releases/latest)
-
+ 
 # Extract the tag name from the release information
 tag_name=$(echo "$latest_release" | grep -o '"tag_name": "[^"]*' | cut -d'"' -f4)
-tag_name_number=$(echo $tag_name | tr -cd '[:digit:]')
-
-# Check if the latest release is newer than the current version and update if necessary
-if [[ "$tag_name_number" > "$current_version_number" ]]; then
+ 
+# Compare major version numbers
+compare_major_version "$tag_name" "$current_version"
+major_version_match=$?
+ 
+# Check if major versions are the same and if the latest release is newer than the current version
+if [[ $major_version_match -eq 0 ]] && [[ "$tag_name" > "$current_version" ]]; then
     echo "A new release is available: $tag_name"
-    cd $file_path
+    cd $runtipi_path
     echo "Backing up current version"
-    if [ ! -d "$file_path/backups" ]; then
-    mkdir -p $file_path/backups
-    fi 
+    if [ ! -d "$runtipi_path/backups" ]; then
+        mkdir -p $runtipi_path/backups
+    fi
     tar -czvf runtipi-backup-$current_version.tar.gz --exclude=media --exclude=backups * 
-    mv runtipi-backup-$current_version.tar.gz $file_path/backups
+    mv runtipi-backup-$current_version.tar.gz $runtipi_path/backups
     echo "Starting update"
-    $file_path/runtipi-cli update latest
+    $runtipi_path/runtipi-cli update latest
 else
-    echo "No new release found"
+    echo "No new release found or major version mismatch"
 fi
